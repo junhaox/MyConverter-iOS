e2c4e52dba619e69ebd89fa3531c1da717587fbc
Junhao Xie <junhaox.k@gmail.com>
2017-01-06 21:02:58 -0800

    Added option to swipe currency cell to remove it from the list

--- UnitConverter/CurrencyViewController.swift
+++ UnitConverter/CurrencyViewController.swift
@@ -95,14 +95,18 @@ class CurrencyViewController: UIViewController, UITableViewDataSource, UITableVi
         }
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let remove = UITableViewRowAction(style: .normal, title: "Remove") { action, indexPath in
+            self.currList.remove(at: indexPath.row)
+            self.tableView.reloadData()
+        }
+        
+        remove.backgroundColor = UIColor.red
+        
+        return [remove]
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+    }
 }
